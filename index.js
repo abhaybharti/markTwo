15bc1d4449dd414c2808a70fba3cceebe4c494b6
Abhay Bharti <abhaybharti@users.noreply.github.com>
2022-02-12 17:11:38 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-var totalQuestionCount = 3;
+var totalQuestionCount = 5;
 var currentQuestionNo = 1;
 var totalScore = 0;
 var wrongAnswerCount = 0;
 var answerSelectedByUser = "";
 var correctAnswerForHtml = "";
 var userName = "";
-var questionNumbers = generateUniqueRandomNumber(3, 3);
+var questionNumbers = generateUniqueRandomNumber(5, 5);
 
-function displayRadioValue() {
-  var ele = document.getElementsByName("answer");
-  for (i = 0; i < ele.length; i++) {
-    if (ele[i].checked) {
-      //document.getElementById
-      console.log(ele[i].value);
-    }
+var userDetailContainer = document.querySelector("#userdetailcontainer");
+var quizContainer = document.querySelector("#quizcontainer");
+
+function displayContainer() {
+  quizContainer.style.display = "none";
+  userDetailContainer.style.display = "block";
+}
+
+function readUserNameAndStartQuiz() {
+  userName = document.getElementById("username");
+  if (userName.value.length === 0) {
+    userDetailContainer.style.display = "block";
+  } else {
+    quizContainer.style.display = "block";
+    userDetailContainer.style.display = "none";
+    updateQuestionOnUi();
   }
 }
 
-function updateQuestionOnUi() {
-  console.log(randomNumber());
-  //var questionBankIndex = randomNumber();
+// function displayRadioValue() {
+//   var ele = document.getElementsByName("answer");
+//   for (i = 0; i < ele.length; i++) {
+//     if (ele[i].checked) {
+//       //document.getElementById
+//       console.log(ele[i].value);
+//     }
+//   }
+// }
 
+function updateQuestionOnUi() {
   var tempQuestionNo = questionNumbers.shift() - 1;
   var questionNoForHtml = questionBank[tempQuestionNo].questionNo;
   var questionForHtml = questionBank[tempQuestionNo].question;
@@ -39,14 +55,15 @@ function updateQuestionOnUi() {
     Then it is creating a variable called answerText1 and setting it equal to the id of the answer
     text.
     Then it is setting the innerHTML of the answer text equal to the first element of the array. */
-  document.getElementById("answerText1").innerHTML = answerListForHtml[0];
+  document.getElementById("answerText1").innerText = answerListForHtml[0];
   document.getElementById("answer1").value = answerListForHtml[0];
-  document.getElementById("answerText2").innerHTML = answerListForHtml[1];
+  document.getElementById("answerText2").innerText = answerListForHtml[1];
   document.getElementById("answer2").value = answerListForHtml[1];
-  document.getElementById("answerText3").innerHTML = answerListForHtml[2];
+  document.getElementById("answerText3").innerText = answerListForHtml[2];
   document.getElementById("answer3").value = answerListForHtml[2];
-  document.getElementById("answerText4").innerHTML = answerListForHtml[3];
+  document.getElementById("answerText4").innerText = answerListForHtml[3];
   document.getElementById("answer4").value = answerListForHtml[3];
+  document.getElementById("selectedAnswser").innerText = "";
   currentQuestionNo++;
 }
 
@@ -54,7 +71,8 @@ function checkAnswerAndLoadNextQuestion() {
   updateAnswer();
   if (correctAnswerForHtml === answerSelectedByUser) {
     totalScore++;
-    document.getElementById("score").innerHTML = totalScore;
+    document.getElementById("score").innerText =
+      "Current Score : " + totalScore;
   } else {
     wrongAnswerCount++;
   }
@@ -68,18 +86,16 @@ function checkAnswerAndLoadNextQuestion() {
 }
 
 function updateAnswer() {
-  document.getElementById("selectedAnswser").innerHTML = "";
+  document.getElementById("selectedAnswser").innerText = "";
   var ele = document.getElementsByName("answer");
   for (var i = 0; i < ele.length; i++) {
     if (ele[i].checked) {
       answerSelectedByUser = ele[i].value;
-      document.getElementById("selectedAnswser").innerHTML =
+      document.getElementById("selectedAnswser").innerText =
         answerSelectedByUser;
     }
   }
 }
-
-function getQuestionAnswerFromQuestionBank() {}
 
 /**
  * Returns a random number between the two numbers that are passed in
@@ -118,32 +134,89 @@ function printTestSummary() {
   quizContainer.style.display = "none";
 
   currentQuestionNo = currentQuestionNo - 1;
-  document.getElementById("resultTitle").innerHTML = "Result : ";
+  document.getElementById("resultTitle").innerText =
+    userName.value + "'s Result : ";
 
-  document.getElementById("finalscore").innerHTML =
+  document.getElementById("finalscore").innerText =
     totalScore + " of " + currentQuestionNo;
 
-  document.getElementById("finalscoreinpercent").innerHTML =
+  document.getElementById("finalscoreinpercent").innerText =
     (totalScore * 100) / currentQuestionNo + "%";
 }
 
 var questionBank = [
   {
     questionNo: "1",
-    question: "What is capital city of India?",
-    answerList: ["Delhi", "Mumbai", "Bengaluru", "Chennai"],
-    correctAnswer: "Delhi",
+    question: "How do you create a function in JavaScript?",
+    answerList: [
+      "function:myfunction()",
+      "function=myfunction()",
+      "function()",
+      "function myFunction()",
+    ],
+    correctAnswer: "function myFunction()",
   },
   {
     questionNo: "2",
-    question: "What is national flag of India called?",
-    answerList: ["Tiranga", "Flag", "Jan Gan Man", "Vande Mataram"],
-    correctAnswer: "Tiranga",
+    question: "How do you call a function named printTestSummary()?",
+    answerList: [
+      "call printTestSummary()",
+      "call function printTestSummary()",
+      "printTestSummary",
+      "printTestSummary()",
+    ],
+    correctAnswer: "printTestSummary()",
   },
   {
     questionNo: "3",
-    question: "What is national animal of India?",
-    answerList: ["Lion", "Tiger", "Elephant", "Snake"],
-    correctAnswer: "Tiger",
+    question: "How to write an IF statement in JavaScript?",
+    answerList: ["IF i  === 5 then", "if i=5 then", "if i=5", "if (i==5)"],
+    correctAnswer: "if (i==5)",
+  },
+  {
+    questionNo: "4",
+    question: "How to call function printTestSummary()?",
+    answerList: [
+      "call printTestSummary()",
+      "call function printTestSummary()",
+      "printTestSummary",
+      "printTestSummary()",
+    ],
+    correctAnswer: "printTestSummary()",
+  },
+  {
+    questionNo: "5",
+    question:
+      'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answerList: ["if i <> 5", "if (i != 5)", "if (i <> 5)", "if i!=5 then"],
+    correctAnswer: "if (i != 5)",
+  },
+  {
+    questionNo: "6",
+    question: "How does a FOR loop start?",
+    answerList: [
+      "for i=1 to 5",
+      "for (i=0; i<5; i++)",
+      "for (i <=5 );",
+      "for each",
+    ],
+    correctAnswer: "for (i=0; i<5; i++)",
+  },
+  {
+    questionNo: "7",
+    question: "How can you add a comment in a JavaScript?",
+    answerList: [
+      "'this is a comment",
+      "<!-- This is a comment -->",
+      "//This is a comment",
+      "#This is a comment",
+    ],
+    correctAnswer: "//This is a comment",
+  },
+  {
+    questionNo: "8",
+    question: "JavaScript is the same as Java.",
+    answerList: ["True", "false", "yes", "NA"],
+    correctAnswer: "false",
   },
 ];
